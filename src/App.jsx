fb266dfbfef5ad96538cf0a150c9d11a11d6f356
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-02-01 02:13:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import './App.css';
 import LikeImage from './components/LikeImage';
+import LikeImageHoc from './components/LikeImageHoc.jsx';
 import LikePost from './components/LikePost';
+import LikePostHoc from './components/LikePostHoc';
+// import ParentCounter from './components/ParentCounter';
 
 function App() {
   return (
     <div>
-      <h3>Some Blog</h3>
+
+      <h1>Some Blog</h1>
+
+{/* solution for render props */}
+
       <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+        <h2>Render Props Method:- </h2>
+        <ParentCounter render={(counter, handleIncreament)=> <LikeImage likeImageCounter={counter} handleLikeImageCount = {handleIncreament}/>}/>
+        <ParentCounter render={(counter, handleIncreament)=><LikePost LikePostCounter={counter} handlePostCount = {handleIncreament}/>}/>
       </div>
+
+
+{/* Solution for HOC */} 
+
+      <div className='buttons'>
+        <h2>Using HOC:-</h2>
+        <LikeImageHoc/>
+        <LikePostHoc/>
+      </div>
+
     </div>
   );
 }
